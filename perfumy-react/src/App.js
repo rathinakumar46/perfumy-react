85ce380a2320e8ea4efc49c80312fbc39c6cfda7
rathinakumar46 <rathinakumar825@gmail.com>
2023-12-22 16:08:45 +0530

    update project

---
+++ perfumy-react/src/App.js
@@ -0,0 +1,20 @@
+import Navbar from './component/Navbar'
+import About from "./component/About"
+import Product from "./component/product"
+import Search from "./component/Search"
+import Footer from "./component/Footer"
+
+
+function App() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Search></Search>
+            <Product></Product>
+            <About></About>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App ;
---
+++ perfumy-react/src/component/product.js
@@ -0,0 +1,33 @@
+
+//Products Component
+function Product()
+{
+  return(
+    <div className="products">
+        <div class="box">
+            <img src="https://source.unsplash.com/220x220/?perfum?1" alt="one"></img>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat vel sapiente minus corrupti quidem,
+                reiciendis perferendis unde. Laborum, similique aut.</p>
+        </div>
+
+        <div class="box">
+            <img src="https://source.unsplash.com/220x220/?perfum?1" alt="two"></img>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat vel sapiente minus corrupti quidem,
+                reiciendis perferendis unde. Laborum, similique aut.</p>
+        </div>
+
+        <div class="box">
+            <img src="https://source.unsplash.com/220x220/?perfum?1" alt="three"></img>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat vel sapiente minus corrupti quidem,
+                reiciendis perferendis unde. Laborum, similique aut.</p>
+        </div>
+
+    </div>
+    
+  )
+}
+
+export default Product ;
